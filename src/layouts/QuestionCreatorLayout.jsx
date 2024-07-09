9febaad3e7f64d92bbf37a5493d9488023bdf3cb
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2024-07-09 22:36:32 +0800

    use question title field in layout

--- src/layouts/QuestionCreatorLayout.jsx
+++ src/layouts/QuestionCreatorLayout.jsx
@@ -1,9 +1,13 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Button, Divider, Grid, Typography } from "@mui/material";
 import MarkdownPreview from "@uiw/react-markdown-preview";
 import React, { useState } from "react";
 import MonacoEditor from "react-monaco-editor";
+import validator from "validator";
+
+import QuestionTitleField from "../inputs/QuestionTitleField";
 
 const QuestionCreatorLayout = () => {
+  const [questionTitle, setQuestionTitle] = useState("");
   const [userInput, setUserInput] = useState("");
 
   // eslint-disable-next-line no-unused-vars
@@ -11,43 +15,88 @@ const QuestionCreatorLayout = () => {
     setUserInput(v);
   };
 
+  const handleQuestionTitle = (e) => {
+    setQuestionTitle(e.target.value);
+  };
+
+  const isValidFormInput =
+    !validator.isEmpty(questionTitle) && !validator.isEmpty(userInput);
+
   const markdown = userInput;
 
   return (
     <Box
       sx={{
-        height: "100vh",
+        // todo: dynamically calculate height based on remaining space on viewport
+        height: "94vh",
         display: "flex",
         flexDirection: "column",
         overflow: "hidden",
       }}
     >
-      <Grid container spacing={2} sx={{ flex: 1, overflow: "hidden" }}>
+      <Grid
+        container
+        spacing={2}
+        sx={{ flex: 1, overflow: "hidden" }}
+        padding={1}
+      >
         <Grid
           item
           xs={6}
           sx={{ display: "flex", flexDirection: "column", height: "100%" }}
         >
-          <Box sx={{ flex: 1, overflow: "hidden" }}>
-            <MonacoEditor
-              language="markdown"
-              value={userInput}
-              onChange={handleUserInput}
-              height="100%"
-              options={{
-                minimap: { enabled: false },
-                automaticLayout: true,
-              }}
-            />
-          </Box>
+          <QuestionTitleField onChange={handleQuestionTitle} />
+          <Typography variant="h6" padding={1}>
+            Enter question description here (Markdown supported)
+          </Typography>
+          <Divider />
+          <MonacoEditor
+            language="markdown"
+            value={userInput}
+            onChange={handleUserInput}
+            height="100%"
+            options={{
+              wordWrap: true,
+              minimap: { enabled: false },
+              automaticLayout: true,
+            }}
+          />
         </Grid>
         <Grid
           item
           xs={6}
           sx={{ display: "flex", flexDirection: "column", height: "100%" }}
         >
-          <Box sx={{ flex: 1, overflow: "auto" }}>
-            <MarkdownPreview source={markdown} />
+          <Typography variant="h6" padding={1}>
+            Question preview
+          </Typography>
+          <Divider />
+          <Box sx={{ flex: 1, overflow: "auto" }} border={1}>
+            <MarkdownPreview
+              source={markdown}
+              wrapperElement={{
+                "data-color-mode": "light",
+              }}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              mt: 2,
+              gap: 1,
+            }}
+          >
+            <Button color="warning" variant="outlined" sx={{ mr: 1 }}>
+              Discard
+            </Button>
+            <Button
+              color="success"
+              variant="contained"
+              disabled={!isValidFormInput}
+            >
+              Submit
+            </Button>
           </Box>
         </Grid>
       </Grid>
