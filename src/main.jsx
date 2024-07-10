4dad8cc7c1fdd6f08588dd3eae955ea64e42f894
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2024-07-10 23:47:31 +0800

    setup local storage mock data

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
+
+import mockQuestions from "./mocks/mockQuestions";
 import App from "./App";
+
+// Setup mock data
+if (localStorage.getItem("questionList") === null) {
+  localStorage.setItem("questionList", JSON.stringify(mockQuestions));
+}
 
 // Application entry point
 createRoot(document.getElementById("root")).render(<App />);
