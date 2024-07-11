ffdea208ef7045b5f558537deb59e3e864304a4c
Siew Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2024-07-11 17:43:01 +0800

    feat: add notification on submit question (#12)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 
 import { MantineProvider } from "@mantine/core";
 
 import AppRouter from "./AppRouter";
+import { Notifications } from "@mantine/notifications";
 
 const App = () => {
   return (
     <MantineProvider>
+      <Notifications position="top-right" />
       <AppRouter />
     </MantineProvider>
   );
