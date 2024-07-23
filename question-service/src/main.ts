f8ecf68b052f0d5da73dbe259d64af85396da92b
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2024-07-23 20:55:29 +0800

    fix cors error

--- question-service/src/main.ts
+++ question-service/src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   // TODO: configure CORS for production
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: 'GET, POST',
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
