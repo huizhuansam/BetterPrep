2e803ace2af8796654263c7affd7a1c76b638931
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2024-07-27 16:40:53 +0800

    fix form accessibility

--- frontend/src/forms/LoginForm.tsx
+++ frontend/src/forms/LoginForm.tsx
@@ -1,8 +1,8 @@
 import {
   Anchor,
   Button,
-  Card,
   Center,
+  Fieldset,
   PasswordInput,
   Text,
   TextInput,
@@ -36,41 +36,39 @@ const LoginForm = () => {
   };
 
   return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder w={viewportWidth / 4}>
-      <Card.Section inheritPadding py="xs">
-        <Center>
-          <Title order={2}>Log in</Title>
-        </Center>
-      </Card.Section>
-      <Card.Section inheritPadding>
-        <Center>
-          <Text size="sm" c="dimmed">
-            New to BetterPrep? <Anchor href="/signup">Sign up instead</Anchor>
-          </Text>
-        </Center>
-      </Card.Section>
-
-      <Card.Section inheritPadding py="xs">
-        <TextInput
-          placeholder="Username"
-          variant="filled"
-          onChange={handleInputUsername}
-        />
-      </Card.Section>
-      <Card.Section inheritPadding py="xs">
-        <PasswordInput
-          placeholder="Password"
-          variant="filled"
-          onChange={handleInputPassword}
-        />
-        <Anchor size="sm">Forgot password?</Anchor>
-      </Card.Section>
-      <Card.Section inheritPadding py="xs">
-        <Button fullWidth disabled={!isFormCompleted} onClick={handleLogin}>
-          Continue
-        </Button>
-      </Card.Section>
-    </Card>
+    <Fieldset radius="md" w={viewportWidth / 4}>
+      <Center>
+        <Title order={2}>Log in</Title>
+      </Center>
+      <Center>
+        <Text size="sm" c="dimmed">
+          New to BetterPrep? <Anchor href="/signup">Sign up instead</Anchor>
+        </Text>
+      </Center>
+      <TextInput
+        mt="md"
+        label="Username"
+        placeholder="Username"
+        variant="filled"
+        onChange={handleInputUsername}
+      />
+      <PasswordInput
+        mt="md"
+        label="Password"
+        placeholder="Password"
+        variant="filled"
+        onChange={handleInputPassword}
+      />
+      <Anchor size="sm">Forgot password?</Anchor>
+      <Button
+        mt="md"
+        fullWidth
+        disabled={!isFormCompleted}
+        onClick={handleLogin}
+      >
+        Continue
+      </Button>
+    </Fieldset>
   );
 };
 
--- frontend/src/forms/SignupForm.tsx
+++ frontend/src/forms/SignupForm.tsx
@@ -1,116 +1,202 @@
 import {
   Anchor,
+  Box,
   Button,
-  Card,
   Center,
+  Checkbox,
+  Fieldset,
   PasswordInput,
+  Popover,
+  Progress,
+  rem,
   Text,
   TextInput,
   Title,
 } from "@mantine/core";
 import { useViewportSize } from "@mantine/hooks";
+import { CheckIcon, Cross1Icon } from "@radix-ui/react-icons";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import validator from "validator";
 
 const SignupForm = () => {
   const navigateTo = useNavigate();
-
   const { width: viewportWidth } = useViewportSize();
 
   const [emailAddress, setEmailAddress] = useState("");
   const [username, setUsername] = useState("");
+  const [isPasswordPopoverOpened, setPasswordPopoverOpened] = useState(false);
   const [password, setPassword] = useState("");
   const [retypedPassword, setRetypedPassword] = useState("");
+  const [isAgreeToTerms, setAgreeToTerms] = useState(false);
 
-  const isFormCompleted =
-    !validator.isEmpty(emailAddress) &&
-    !validator.isEmpty(username) &&
-    !validator.isEmpty(password) &&
-    !validator.isEmpty(retypedPassword);
+  // password validation
+  const passwordRequirements = [
+    { re: /[0-9]/, label: "Includes number" },
+    { re: /[a-z]/, label: "Includes lowercase letter" },
+    { re: /[A-Z]/, label: "Includes uppercase letter" },
+    { re: /[$&+,:;=?@#|'<>.^*()%!-]/, label: "Includes special symbol" },
+  ];
+
+  const getPasswordStrength = (password: string) => {
+    let multiplier = password.length > 5 ? 0 : 1;
+    passwordRequirements.forEach((requirement) => {
+      if (!requirement.re.test(password)) {
+        multiplier += 1;
+      }
+    });
+    return Math.max(
+      100 - (100 / (passwordRequirements.length + 1)) * multiplier,
+      10
+    );
+  };
+
+  const PasswordRequirement = ({
+    meets,
+    label,
+  }: {
+    meets: boolean;
+    label: string;
+  }) => {
+    return (
+      <Text
+        c={meets ? "teal" : "red"}
+        style={{ display: "flex", alignItems: "center" }}
+        mt={7}
+        size="sm"
+      >
+        {meets ? (
+          <CheckIcon style={{ width: rem(14), height: rem(14) }} />
+        ) : (
+          <Cross1Icon style={{ width: rem(14), height: rem(14) }} />
+        )}{" "}
+        <Box ml={10}>{label}</Box>
+      </Text>
+    );
+  };
+
+  const passwordChecks = passwordRequirements.map((requirement, index) => (
+    <PasswordRequirement
+      key={index}
+      label={requirement.label}
+      meets={requirement.re.test(password)}
+    />
+  ));
+  const passwordStrength = getPasswordStrength(password);
+  const passwordStrengthColor =
+    passwordStrength === 100
+      ? "teal"
+      : passwordStrength > 50
+      ? "yellow"
+      : "red";
+
+  // form validation
   const isUsernameValid =
     validator.isLength(username, { min: 1 }) &&
     validator.isAlphanumeric(username);
   const isPasswordsMatch = password === retypedPassword;
-  const isPasswordValid = validator.isStrongPassword(password);
+  const isPasswordValid = passwordStrength === 100;
   const isEmailValid = validator.isEmail(emailAddress);
   const isFormInputValid =
-    isFormCompleted &&
+    isAgreeToTerms &&
     isPasswordsMatch &&
     isPasswordValid &&
     isEmailValid &&
     isUsernameValid;
 
-  const handleInputEmailAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmailAddress(e.target.value);
-  };
-
-  const handleInputUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  };
-
-  const handleInputPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleRetypedPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRetypedPassword(e.target.value);
-  };
-
+  // form actions
   const handleSignUp = () => {
     navigateTo("/questions");
   };
 
   return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder w={viewportWidth / 4}>
-      <Card.Section inheritPadding py="xs">
-        <Center>
-          <Title order={2}>Sign up</Title>
-        </Center>
-      </Card.Section>
-      <Card.Section inheritPadding>
-        <Center>
-          <Text size="sm" c="dimmed">
-            Have an existing BetterPrep account?{" "}
-            <Anchor href="/login">Log in</Anchor>
+    <Fieldset radius="md" w={viewportWidth / 4}>
+      <Center>
+        <Title order={2}>Sign up</Title>
+      </Center>
+      <Center>
+        <Text size="sm" c="dimmed">
+          Have an existing BetterPrep account?{" "}
+          <Anchor href="/login">Log in</Anchor>
+        </Text>
+      </Center>
+      <TextInput
+        mt="md"
+        label="Email address"
+        placeholder="Email address"
+        variant="filled"
+        onChange={(e) => setEmailAddress(e.target.value)}
+        error={!isEmailValid ? "Invalid email address" : false}
+      />
+      <TextInput
+        mt="md"
+        label="Username"
+        placeholder="Username"
+        variant="filled"
+        onChange={(e) => setUsername(e.target.value)}
+        error={!isUsernameValid ? "Username must be alphanumeric" : false}
+      />
+      <Popover
+        opened={isPasswordPopoverOpened}
+        position="bottom"
+        width="target"
+        transitionProps={{ transition: "pop" }}
+      >
+        <Popover.Target>
+          <div
+            onFocusCapture={() => setPasswordPopoverOpened(true)}
+            onBlurCapture={() => setPasswordPopoverOpened(false)}
+          >
+            <PasswordInput
+              mt="md"
+              label="Password"
+              placeholder="Password"
+              variant="filled"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </Popover.Target>
+        <Popover.Dropdown>
+          <Progress
+            color={passwordStrengthColor}
+            value={passwordStrength}
+            size={5}
+            mb="xs"
+          />
+          <PasswordRequirement
+            label="Includes at least 6 characters"
+            meets={password.length > 5}
+          />
+          {passwordChecks}
+        </Popover.Dropdown>
+      </Popover>
+      <PasswordInput
+        mt="md"
+        label="Re-type password"
+        placeholder="Re-type password"
+        variant="filled"
+        onChange={(e) => setRetypedPassword(e.target.value)}
+      />
+      <Checkbox
+        checked={isAgreeToTerms}
+        mt="md"
+        label={
+          <Text>
+            {/* TODO: write terms of service */}I agree to the{" "}
+            <Anchor href="/">terms of service</Anchor>
           </Text>
-        </Center>
-      </Card.Section>
-
-      <Card.Section inheritPadding py="xs">
-        <TextInput
-          placeholder="Email address"
-          variant="filled"
-          onChange={handleInputEmailAddress}
-        />
-      </Card.Section>
-      <Card.Section inheritPadding py="xs">
-        <TextInput
-          placeholder="Username"
-          variant="filled"
-          onChange={handleInputUsername}
-        />
-      </Card.Section>
-      <Card.Section inheritPadding py="xs">
-        <PasswordInput
-          placeholder="Password"
-          variant="filled"
-          onChange={handleInputPassword}
-        />
-      </Card.Section>
-      <Card.Section inheritPadding py="xs">
-        <PasswordInput
-          placeholder="Re-type password"
-          variant="filled"
-          onChange={handleRetypedPassword}
-        />
-      </Card.Section>
-      <Card.Section inheritPadding py="xs">
-        <Button fullWidth disabled={!isFormInputValid} onClick={handleSignUp}>
-          Create account
-        </Button>
-      </Card.Section>
-    </Card>
+        }
+        onChange={(event) => setAgreeToTerms(event.currentTarget.checked)}
+      />
+      <Button
+        mt="md"
+        fullWidth
+        disabled={!isFormInputValid}
+        onClick={handleSignUp}
+      >
+        Create Account
+      </Button>
+    </Fieldset>
   );
 };
 
