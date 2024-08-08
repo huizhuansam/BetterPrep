0398c2669c999f6b26a8678759e13066a44892bd
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2024-08-08 15:24:41 +0800

    add logout routes

--- gateway-service/router/router.go
+++ gateway-service/router/router.go
@@ -13,10 +13,12 @@ func SetupRoutes(app *fiber.App) {
 	authRouter := api.Group("/auth")
 	authRouter.Post("/signup", handler.Signup)
 	authRouter.Post("/login", handler.Login)
+	authRouter.Use(middleware.Protected()).Post("/logout", handler.Logout)
+	authRouter.Get("/me", handler.Me)
 
 	// question
-	questionRouter := api.Group("/questions")
-	questionRouter.Post("/", middleware.Protected(), handler.AddQuestion)
-	questionRouter.Get("/", middleware.Protected(), handler.GetQuestions)
-	questionRouter.Get("/:slug", middleware.Protected(), handler.GetQuestion)
+	questionRouter := api.Group("/questions").Use(middleware.Protected())
+	questionRouter.Post("/", handler.AddQuestion)
+	questionRouter.Get("/", handler.GetQuestions)
+	questionRouter.Get("/:slug", handler.GetQuestion)
 }
