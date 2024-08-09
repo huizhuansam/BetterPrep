855bd1f4f5ddee8f54ec6389d9a38d026ae9d0c3
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2024-08-09 11:36:43 +0800

    hello world!

--- profile-service/src/main/java/com/betterprep/profile_service/ProfileServiceApplication.java
+++ profile-service/src/main/java/com/betterprep/profile_service/ProfileServiceApplication.java
@@ -2,11 +2,17 @@ package com.betterprep.profile_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class ProfileServiceApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileServiceApplication.class, args);
