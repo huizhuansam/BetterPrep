41d4111f6f272c2dab76b49f636a2328aba0aeb6
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2024-08-16 13:13:34 +0800

    add profile entity

---
+++ profile-service/src/main/java/com/betterprep/profile_service/profile/Profile.java
@@ -0,0 +1,39 @@
+package com.betterprep.profile_service.profile;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "profiles")
+public class Profile {
+
+  @Id
+  private String username;
+  private String email;
+
+  public Profile() {
+  }
+
+  public Profile(String username, String email) {
+    this.username = username;
+    this.email = email;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+}
