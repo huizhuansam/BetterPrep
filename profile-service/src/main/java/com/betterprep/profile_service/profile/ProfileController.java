8b97c1f2a920ae076ce890d119cb25e992412f40
Siew Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2024-08-16 13:23:26 +0800

    feat: profile service (#23)

---
+++ profile-service/src/main/java/com/betterprep/profile_service/profile/ProfileController.java
@@ -0,0 +1,25 @@
+package com.betterprep.profile_service.profile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/profiles")
+public class ProfileController {
+  @Autowired
+  private ProfileService profileService;
+
+  @GetMapping
+  public List<Profile> getAllProfiles() {
+    return profileService.getAllProfiles();
+  }
+
+  @PostMapping
+  public Profile addProfile(@RequestBody Profile profile) {
+    return profileService.addProfile(profile);
+  }
+
+}
---
+++ profile-service/src/main/java/com/betterprep/profile_service/profile/ProfileService.java
@@ -0,0 +1,20 @@
+package com.betterprep.profile_service.profile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProfileService {
+  @Autowired
+  private ProfileRepository profileRepository;
+
+  public List<Profile> getAllProfiles() {
+    return profileRepository.findAll();
+  }
+
+  public Profile addProfile(Profile profile) {
+    return profileRepository.save(profile);
+  }
+}
